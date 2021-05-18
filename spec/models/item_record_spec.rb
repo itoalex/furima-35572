c40d9312cc9466fb6ea7293799fc56b50c037a67
Alexsensi <hiwork98ss@gmail.com>
2021-05-18 16:13:03 +0900

    必要ない記述を削除

--- spec/models/item_record_spec.rb
+++ spec/models/item_record_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ItemRecord, type: :model do
-  context '内容に問題ない場合' do
-    it 'tokenがあれば保存ができること' do
-      expect(@order).to be_valid
-    end
-  end
 
-  context '内容に問題がある場合' do
-    it 'tokenが空では登録できないこと' do
-      @order.token = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Token can't be blank")
-    end
-  end
 end
